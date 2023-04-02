7ef279b1b3bf7cb8f606eb9c4dac52639f095fda
Kiyong Nam <moosin76@gmail.com>
2023-04-02 17:00:01 +0900

    first commit

---
+++ src-ssr/middlewares/session.js
@@ -0,0 +1,22 @@
+import { ssrMiddleware } from 'quasar/wrappers'
+import session from 'express-session';
+import FileStore from 'session-file-store';
+
+export default ssrMiddleware(({ app, resolve, render, serve }) => {
+	const SessionStore = FileStore(session);
+
+	app.use(session({
+		key : 'connect.sid',
+		store : new SessionStore({}),
+		secret : 'keyboard cat',
+		resave : false,
+		saveUninitialized : true,
+	}));
+
+	app.use('*', (req, res,next)=>{
+		app.set('serviceURL', `${req.protocol}://${req.headers.host}`);
+		console.log('session', req.session);
+		next();
+	})
+
+})
